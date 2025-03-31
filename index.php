f01f53051c0dd6569ec8934c7371fc9d7dc2743c
tbeaumont79 <tbeaumont79@icloud.com>
2025-03-31 17:04:21 +0200

    feat: init the project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="h-screen bg-red-500 flex justify-center items-center">
+        <nav> 
+            <ul>
+                <li><a href="#">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
