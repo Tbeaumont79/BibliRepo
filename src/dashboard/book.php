ab771013c5fba1fadf7bf665620c82a85404fe19
tbeaumont79 <tbeaumont79@icloud.com>
2025-04-10 12:48:37 +0200

    feat(delete book: adming panel): the delete book work great

--- src/dashboard/book.php
+++ src/dashboard/book.php
@@ -1,33 +1,55 @@
 <?php
 require('./databases/db_connect.php');
-
-$stmt = $connect->query("SELECT * FROM book");
-if ($stmt->num_rows > 0) {
-  $books = [];
-  for ( $i = 0; $i < $stmt->num_rows; $i++) {
-    array_push($books,$stmt->fetch_assoc());
-  }
+$books = [];
+$is_fetched = false;
+if ($is_fetched == false) {
+    $books = loadBook($connect);
+    $is_fetched = true;
+}
+function loadBook($connect)
+{
+    $books = [];
+    $stmt = $connect->query("SELECT * FROM book");
+    if ($stmt->num_rows > 0) {
+        for ($i = 0; $i < $stmt->num_rows; $i++) {
+            array_push($books, $stmt->fetch_assoc());
+        }
+    }
+    return $books;
 }
 if (isset($_POST['create'])) {
-    $books.array_push(addBook($connect));
+    $books = [];
+    addBook($connect);
+    $books = loadBook($connect);
+
+}
+
+if (isset($_POST['delete']) && isset($_POST['book_id'])) {
+    $books = [];
+    deleteBook($connect);
+    $books = loadBook($connect);
+}
+function deleteBook($connect)
+{
+    $id = $_POST['book_id'];
+    $stmt = $connect->query("DELETE FROM book WHERE book.id = " . $id . "");
+    if (!$stmt) {
+        throw new Exception("Error deleting book in databases");
+    }
+
 }
 function addBook($connect)
 {
     if (isset($_POST['title']) && isset($_POST['author']) && isset($_POST['category'])) {
-        print_r("je passe ici 1");
         $title = $_POST['title'];
         $author = $_POST['author'];
         $category = $_POST['category'];
 
         $stmt = $connect->query('INSERT INTO book (title, author, category) VALUES ("' . $title . '", "' . $author . '", "' . $category . '")');
-
-        if ($stmt->num_rows > 0) {
-            $books = $stmt->fetch_assoc();
-            return $books;
+        if (!$stmt) {
+            throw new Exception("Error adding book in database");
         }
     }
-
-    return throw new Exception("Error adding a book in the Database");
 }
 ?>
 
@@ -95,29 +117,34 @@ function addBook($connect)
                   </span>
                 </a>
               </th>
-              <th scope="col" class="relative py-3.5 pr-0 pl-3">
-                <span class="sr-only">Edit</span>
-              </th>
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-200 bg-white">
            <?php
-            for ($i = 0; $i < count($books);  $i++) {
-                ?>
+           if ($books && count($books) > 0) {
+               for ($i = 0; $i < count($books);  $i++) {
+                   ?>
             <tr>
                 <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><?php echo $books[$i]['id'] ?></td>
                 <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><?php echo $books[$i]['title'] ?></td>
                 <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><?php echo $books[$i]['author'] ?></td>
                 <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><?php echo $books[$i]['category'] ?></td>
-              <td class="relative py-4 pr-4 pl-3 text-right text-sm whitespace-nowrap sm:pr-0">
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                <td class="relative py-4 pr-4 pl-3 text-right text-sm whitespace-nowrap sm:pr-0">
+                  <th scope="col" class="relative py-3.5 pr-0 pl-3">
+                    <form action="book.php" method="POST">
+                      <span class="sr-only">Delete</span>
+                      <input type="hidden" name="book_id" value="<?php echo $books[$i]['id']; ?>" />
+                      <button type="submit" class="text-indigo-600 hover:text-indigo-900" name="delete">Delete</button>
+                    </form>
+                  </th>
                 </td>
               </tr>
           
-            <?php } ?>
+            <?php }
+               } ?>
             <tr>
                 <form action="book.php" method="POST">
-                  <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><?php echo $i + 1?></td>
+                  <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><?php echo "id" ?></td>
                   <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><input class="border-1 rounded-md border-purple-600 p-2" type="text" name="title" placeholder="Title"/></td>
                   <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><input class="border-1 rounded-md border-purple-600 p-2" type="text" name="author" placeholder="Author"/></td>
                   <td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"><input class="border-1 rounded-md border-purple-600 p-2" type="text" name="category" placeholder="Category"/></td>
