669283cb594e753cb801e1e9a566fcca6c7c92f4
tbeaumont79 <tbeaumont79@icloud.com>
2025-04-11 15:23:45 +0200

    feat: update style remove right border

--- app/dashboard/index.php
+++ app/dashboard/index.php
@@ -234,9 +234,7 @@
     </div>
   </main>
 
-  <aside class="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
-    <!-- Secondary column (hidden on smaller screens) -->
-  </aside>
+ 
 </div>
 
     </aside>
